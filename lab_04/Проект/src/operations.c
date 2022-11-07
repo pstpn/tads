18fa718764f9f9919523953956a164added0b9ac
psa21u347 <stepaha78@gmail.com>
2022-11-07 11:01:58 +0300

    Update method

--- lab_04/Проект/src/operations.c
+++ lab_04/Проект/src/operations.c
@@ -1,3 +1,4 @@
+#include "../inc/arr_stack_funcs.h"
 #include "../inc/list_stack_funcs.h"
 #include "../inc/my_types.h"
 #include "../inc/my_err.h"
@@ -7,38 +8,52 @@ int get_postfix_expression_arr_stack(char *in_expr, int len, arr_stack_t *stack,
 {
     int ind_str = 0;
 
+    char cur_elem;
+
 
     for (int i = 0; i < len; ++i)
     {
         if (in_expr[i] == '(')
-            stack->content[stack->len++] = in_expr[i];
+            push_arr_stack(stack, in_expr[i]);
         else if (in_expr[i] == ')')
         {
-            while (stack->len && stack->content[stack->len - 1] != '(')
-                out_expr[ind_str++] = stack->content[--stack->len];
+            pop_arr_stack(stack, &cur_elem);
 
-            if (!stack->len)
+            while (stack->len && cur_elem != '(')
+            {
+                out_expr[ind_str++] = cur_elem;
+                pop_arr_stack(stack, &cur_elem);
+            }
+
+            if (!stack->len && cur_elem != '(')
                 return ERR_EXPRESSION;
-
-            --stack->len;
         }
         else if (in_expr[i] == '+' || in_expr[i] == '-')
         {
-            while (stack->len &&
-            stack->content[stack->len - 1] != '(')
-                out_expr[ind_str++] = stack->content[--stack->len];
+            pop_arr_stack(stack, &cur_elem);
+
+            while (stack->len && cur_elem != '(')
+            {
+                out_expr[ind_str++] = cur_elem;
+                pop_arr_stack(stack, &cur_elem);
+            }
             
-            stack->content[stack->len++] = in_expr[i];
+            push_arr_stack(stack, cur_elem);
+            push_arr_stack(stack, in_expr[i]);
         }
         else if (in_expr[i] == '*' || in_expr[i] == '/')
         {
-            while (stack->len &&
-            stack->content[stack->len - 1] != '(' &&
-            stack->content[stack->len - 1] != '+' &&
-            stack->content[stack->len - 1] != '-')
-                out_expr[ind_str++] = stack->content[--stack->len];
+            pop_arr_stack(stack, &cur_elem);
+
+            while (stack->len && cur_elem != '(' &&
+            cur_elem != '+' && cur_elem != '-')
+            {
+                out_expr[ind_str++] = cur_elem;
+                pop_arr_stack(stack, &cur_elem);
+            }
             
-            stack->content[stack->len++] = in_expr[i];
+            push_arr_stack(stack, cur_elem);
+            push_arr_stack(stack, in_expr[i]);
         }
         else
         {
@@ -50,8 +65,12 @@ int get_postfix_expression_arr_stack(char *in_expr, int len, arr_stack_t *stack,
         }
     }
 
-    while (stack->len)
-        out_expr[ind_str++] = stack->content[--stack->len];
+    do
+    {
+        pop_arr_stack(stack, &cur_elem);
+        if (cur_elem != '(' && cur_elem != ')')
+            out_expr[ind_str++] = cur_elem;
+    } while (stack->len);
 
     return SUCCESS;
 }
@@ -70,36 +89,42 @@ int get_postfix_expression_list_stack(char *in_expr, int len, list_stack_t *stac
             push_list_stack(stack, in_expr[i]);
         else if (in_expr[i] == ')')
         {
-            while (stack->len && stack->top->item != '(')
+            pop_list_stack(stack, &pop_elem);
+
+            while (stack->len && pop_elem != '(')
             {
-                pop_list_stack(stack, &pop_elem);
                 out_expr[ind_str++] = pop_elem;
+                pop_list_stack(stack, &pop_elem);
             }
 
-            if (!stack->len)
+            if (!stack->len && pop_elem != '(')
                 return ERR_EXPRESSION;
-
-            pop_list_stack(stack, &pop_elem);
         }
         else if (in_expr[i] == '+' || in_expr[i] == '-')
         {
-            while (stack->len && stack->top->item != '(')
+            pop_list_stack(stack, &pop_elem);
+
+            while (stack->len && pop_elem != '(')
             {
-                pop_list_stack(stack, &pop_elem);
                 out_expr[ind_str++] = pop_elem;
+                pop_list_stack(stack, &pop_elem);
             }
             
+            push_list_stack(stack, pop_elem);
             push_list_stack(stack, in_expr[i]);
         }
         else if (in_expr[i] == '*' || in_expr[i] == '/')
         {
-            while (stack->len && stack->top->item != '(' &&
-            stack->top->item != '+' && stack->top->item != '-')
+            pop_list_stack(stack, &pop_elem);
+
+            while (stack->len && pop_elem != '(' &&
+            pop_elem != '+' && pop_elem != '-')
             {
-                pop_list_stack(stack, &pop_elem);
                 out_expr[ind_str++] = pop_elem;
+                pop_list_stack(stack, &pop_elem);
             }
             
+            push_list_stack(stack, pop_elem);
             push_list_stack(stack, in_expr[i]);
         }
         else
@@ -112,14 +137,12 @@ int get_postfix_expression_list_stack(char *in_expr, int len, list_stack_t *stac
         }
     }
 
-    while (stack->len)
+    do
     {
         pop_list_stack(stack, &pop_elem);
-        if (pop_elem == '(')
-            return ERR_EXPRESSION;
-            
-        out_expr[ind_str++] = pop_elem;
-    }
+        if (pop_elem != '(' && pop_elem != ')')
+            out_expr[ind_str++] = pop_elem;
+    } while (stack->len);
 
     return SUCCESS;
 }
